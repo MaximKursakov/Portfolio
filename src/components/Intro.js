1d30d619eb6b30f2f5babe538ff5a5084f67cccc
maxim <maximcarlos@gmx.de>
2022-11-30 12:12:53 +0100

    intro leaves sticky positioning when main section is in view

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,44 +8,38 @@ import  {  Card}  from "./Projects-Stack"
 import { Intro1 } from "./Intro1"
 
 
-export function Intro ({introLevel, blackBirdVisibility, whiteBirdVisibility}) {
-    
+export function Intro ({introLevel, blackBirdVisibility, whiteBirdVisibility, mainIsInView}) {
     return(
-        <div className="intro-body"> 
-        <div className="intro-container">
-                <motion.div
-                className="intro-hero"
-                initial={{background: `linear-gradient(90deg, #EDF6FF 50%,  #6DAFFE 50%)`}}
-                animate={{background: `linear-gradient(${introLevel >= 3 ? 180 : 90}deg, #EDF6FF 50%, #6DAFFE 50%)`}}
-                // animate={{x : introLevel === 4 && "-100vw"}}
-                // style={{ scaleX: scrollYProgress}}
-                transition={{duration: 1}}
-                >
-                <div className="intro">
-                    <IntroSidebar></IntroSidebar>
-                    <Bird introLevel={introLevel} visibility={blackBirdVisibility} color="#6DAFFE"></Bird>
-                    <Bird introLevel={introLevel} visibility={whiteBirdVisibility} color="#EDF6FF"></Bird>     
-                    <AnimatePresence> 
-                        {introLevel === 1 ?
-                        <Intro1 introLevel={introLevel}/>
-                        : null}                       
-                    </AnimatePresence>  
-                    <AnimatePresence>
-                        {introLevel === 2 ?
-                        <Intro2></Intro2>
-                        : null}
-                    </AnimatePresence>
-                    <AnimatePresence>
-                        {introLevel === 3 ?
-                        <Card></Card>
-                        : null}
-                    </AnimatePresence>
+        <div className="intro-body" style={{position: !mainIsInView ? "fixed" : "absolute", top: !mainIsInView ? "0" : "100vh"}}> 
+            <div className="intro-container">
+                    <motion.div
+                    className="intro-hero"
+                    initial={{background: `linear-gradient(90deg, #EDF6FF 50%,  #6DAFFE 50%)`}}
+                    animate={{background: `linear-gradient(${introLevel >= 3 ? 180 : 90}deg, #EDF6FF 50%, #6DAFFE 50%)`}}
+                    transition={{duration: 1}}
+                    >
+                    <div className="intro">
+                        <IntroSidebar></IntroSidebar>
+                        <Bird introLevel={introLevel} visibility={blackBirdVisibility} color="#6DAFFE"></Bird>
+                        <Bird introLevel={introLevel} visibility={whiteBirdVisibility} color="#EDF6FF"></Bird>     
+                        <AnimatePresence> 
+                            {introLevel === 1 ?
+                            <Intro1 introLevel={introLevel}/>
+                            : null}                       
+                        </AnimatePresence>  
+                        <AnimatePresence>
+                            {introLevel === 2 ?
+                            <Intro2></Intro2>
+                            : null}
+                        </AnimatePresence>
+                        <AnimatePresence>
+                            {introLevel === 3 ?
+                            <Card></Card>
+                            : null}
+                        </AnimatePresence>
+                    </div>
+                </motion.div>
             </div>
-            </motion.div>
-            
         </div>
-        
-        </div>
-        
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { motion, useScroll } from "framer-motion";
+import { motion, useInView, useScroll } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { Intro } from "./Intro";
 
@@ -6,20 +6,11 @@ export function Main() {
     const [introLevel, setIntroLevel] = useState(1)
     const [blackBirdVisibility, setBlackBirdVisibility] = useState(true)
     const [whiteBirdVisibility, setWhiteBirdVisibility] = useState(false)
-        
-    
-    
-    // function forwardIntro() {
-    //     if(introLevel < 4) {
-    //         setIntroLevel(introLevel + 1)
-    //         setBlackBirdVisibility(!blackBirdVisibility)
-    //         setWhiteBirdVisibility(!whiteBirdVisibility)
-    //     }
-    //     else if(introLevel === 3) {
-    //     }    
-    // }
-
+    const [introSticky, setIntroSticky] = useState(true)
     const ref = useRef(null);
+    const mainRef = useRef(null)
+    const mainIsInView = useInView(mainRef)
+    
     const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["end end", "start start"]
@@ -37,19 +28,21 @@ export function Main() {
             setBlackBirdVisibility(!blackBirdVisibility)
             setWhiteBirdVisibility(!whiteBirdVisibility)
         }
-        else if(scrollYProgress.current < 0.33) {
+        if(scrollYProgress.current < 0.33 && scrollYProgress.current > 0) {
             setIntroLevel(3)
             setBlackBirdVisibility(true)
-            setWhiteBirdVisibility(false)}
-    })
+            setWhiteBirdVisibility(false)
+        }
+        })
     }, [])
+
 
     return(
         <div className="main-container" >
-            <Intro introLevel={introLevel} blackBirdVisibility={blackBirdVisibility} whiteBirdVisibility={whiteBirdVisibility}></Intro>
+            <Intro introLevel={introLevel} blackBirdVisibility={blackBirdVisibility} whiteBirdVisibility={whiteBirdVisibility} mainIsInView={mainIsInView}></Intro>
             <div className="intro-background" ref={ref}>
             </div>
-            <div className="main">
+            <div className="main" ref={mainRef}>
                 main
             </div>
         </div>
