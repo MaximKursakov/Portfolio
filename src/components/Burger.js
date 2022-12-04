8b6f5c395f2c3179066f1dde28968f3463e455f9
maxim <maximcarlos@gmx.de>
2022-12-04 12:25:14 +0100

    burger icon open and closing animation

---
+++ src/components/Burger.js
@@ -0,0 +1,26 @@
+import { AnimatePresence, motion } from "framer-motion";
+
+export function Burger ({menuOpen, setMenuOpen}) {
+    return(
+        <>
+            <motion.div 
+                    className="burger" 
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    animate={{x :menuOpen ? "350%" : "0%"}}
+                    transition={{duration: .3}}
+                    >
+                       <svg width="57" height="41" viewBox="0 0 57 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="Burger">
+                            <motion.line id="Line 1" animate={{y2: menuOpen? 38.5 : 2.5}} transition={{duration: .5}} x1="2.5" y1="2.5" x2="54.5" y2="2.5" stroke="#323537" stroke-width="5" stroke-linecap="round"/>
+                            <AnimatePresence>
+                                {!menuOpen ? 
+                                <motion.line id="Line 2" animate={{pathLength: 1}} exit={{pathLength: 0}} transition={{duration: .5}} y1="20.5" x2="54.5" y2="20.5" stroke="#323537" stroke-width="5" stroke-linecap="round"/>
+                            :null}
+                            </AnimatePresence>
+                            <motion.line id="Line 3" animate={{y2: menuOpen? 2.5 : 38.5}} transition={{duration: .5}} x1="2.5" y1="38.5" x2="54.5" y2="38.5" stroke="#323537" stroke-width="5" stroke-linecap="round"/>
+                            </g>
+                        </svg>
+            </motion.div>
+        </>
+    )
+}
--- src/components/intro-sidebar.js
+++ src/components/intro-sidebar.js
@@ -1,10 +1,16 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
-import {VscMenu} from "react-icons/vsc";
+import {IoIosMenu} from "react-icons/io";
+import { Burger } from "./Burger";
 
 export function IntroSidebar() {
     const [menuOpen, setMenuOpen] = useState(false)
     console.log(menuOpen)
+    const linkAnimation = {
+        initial : {opacity: 0, scale: 0},
+        animate: {opacity:1, scale: 1 , transition: {duration: .3}},
+        exit: {opacity: 0, scale: 0}
+    }
     return(
         <div className="intro-sidebar"> 
         <p className="portfolio-title">MK</p>
@@ -12,12 +18,28 @@ export function IntroSidebar() {
             
             <motion.div 
             className="line1" 
-            animate={{paddingBottom: menuOpen ? "15vh" : "35vh"}}
+            animate={{paddingBottom: menuOpen ? "25vh" : "35vh"}}
             ></motion.div>
-            <div className="burger" onClick={() => setMenuOpen(!menuOpen)}> <VscMenu/> </div>
+            <div className="navMenu">
+                <AnimatePresence>
+                    {menuOpen ? 
+                    <motion.ul className="navLinks"
+                    variants={linkAnimation}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    >
+                        <motion.li>ABOUT</motion.li>
+                        <motion.li>PROJECTS</motion.li>
+                        <motion.li>CONTACT</motion.li>
+                    </motion.ul> : null}
+                    
+                </AnimatePresence>
+                <Burger menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            </div>
             <motion.div 
             className="line2" 
-            animate={{paddingTop: menuOpen ? "15vh" : "35vh"}}
+            animate={{paddingTop: menuOpen ? "25vh" : "35vh"}}
             ></motion.div>
         </div>
         </div>
