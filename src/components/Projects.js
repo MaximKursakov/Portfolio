fc7c202d8449c3f8d39fcc06f0a61093d2945c7c
maxim <maximcarlos@gmx.de>
2022-12-06 17:19:22 +0100

    title- and text adjustments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,16 +22,16 @@ export function Projects() {
     }
 
     const projectArray = [{
-        title: "Project 1", subtitle: "Ecommerce Website", imageTitle:"Project0", techstack: ["HTML", "CSS", "REACT", "MAPBOX"], websiteLink: "https://maximkursakov.github.io/shop", codeLink: "https://github.com/MaximKursakov/shop", id: 1, description: "This Projected started of as a simple Basket and evolved into an entire Frontend for a Fictional Chair Shop. I chose a minimalistic design approach with a Monochromatic Color Scheme and lots of white space to fit the chairs clean style."
+        title: "Project 1", subtitle: "Ecommerce Website", imageTitle:"Project0", techstack: ["HTML", "CSS", "REACT", "MAPBOX"], websiteLink: "https://maximkursakov.github.io/shop", codeLink: "https://github.com/MaximKursakov/shop", id: 1, description: "This Projected started off as a simple basket and evolved into an entire frontend for a fictional Chair Shop. I chose a minimalistic design approach with a monochromatic color scheme and lots of white space to fit the chair's clean style."
     },
     {
-        title: "Project 2", subtitle: "Weather App", imageTitle:"Project1", techstack: ["HTML", "CSS", "JAVASCRIPT"], websiteLink: "https://maximkursakov.github.io/Weather-App", codeLink: "https://github.com/MaximKursakov/Weather-App", id: 2, description: "The Weather App fetches data depending on the Users Location / Input and Displays it accordingly. The Websites Design changes together with certain weather conditions."
+        title: "Project 2", subtitle: "Weather App", imageTitle:"Project1", techstack: ["HTML", "CSS", "JAVASCRIPT"], websiteLink: "https://maximkursakov.github.io/Weather-App", codeLink: "https://github.com/MaximKursakov/Weather-App", id: 2, description: "The Weather App fetches data depending on the users location / input and displays it accordingly. The websites design changes together with certain weather conditions."
     },
     {
-        title: "Project 3", subtitle: "Personal Website", imageTitle:"Project2", techstack: ["HTML", "CSS", "REACT", "FRAMER-MOTION"], websiteLink: "", codeLink: "https://github.com/MaximKursakov/Portfolio", id: 3, description: "With my Portfolio I my wanted to bring in more life and creativity into my website through its design and the usage of animations.Those are beeing used more often while stil keeping the Design simple and clean."
+        title: "Project 3", subtitle: "Personal Website", imageTitle:"Project2", techstack: ["HTML", "CSS", "REACT", "FRAMER-MOTION"], websiteLink: "", codeLink: "https://github.com/MaximKursakov/Portfolio", id: 3, description:"The focus of my portfolio laid on the usage of animations to bring some life and personality into the website, while keeping the design clean and simple."
     },
     {
-        title: "Project 4", subtitle: "DistractLess", imageTitle:"Project3", techstack: [], websiteLink: "", codeLink: "", id: 4, description: "In Progress: Chrome extension which adds aids to block off distractions such as: Blocking whole websites during a certain time period or after defined amount of usage, blocking parts of youtube such as the recommended or the comment section etc."
+        title: "Project 4", subtitle: "DistractLess", imageTitle:"Project3", techstack: [], websiteLink: "", codeLink: "", id: 4, description: "In Progress: Chrome extension which adds aids to block off distractions through functionalities such as: Blocking whole websites during a certain time period or after a defined amount of usage, blocking parts of youtube such as the recommended or the comment section etc."
     }]
 
 
